0ef4aae779505ebce99a9bfa5a1e1dc769c77d8b
Ranjeet Vishwakarma <ranjeetv.dev@gmail.com>
2026-04-21 15:12:22 +0530

    Added the validation for Database_URL

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,11 @@ import "dotenv/config";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(process.env.DATABASE_URL);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is required");
+}
 
+const sql = neon(process.env.DATABASE_URL);
 const db = drizzle(sql);
 
 export { sql, db };
