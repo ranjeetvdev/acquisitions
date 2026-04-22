29bea37f4a3ca107521f386adb5e8a0c2e68a28d
Ranjeet Vishwakarma <ranjeetv.dev@gmail.com>
2026-04-22 10:40:13 +0530

    Implemented logger and middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 import express from "express";
+import logger from "#config/logger.js";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+app.use(helmet());
+app.use(cors());
 
-app.get("/", (req, res) => res.status(200).send("Hello from Acquisitions!"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan("combined", {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  }),
+);
+
+app.get("/", (req, res) => {
+  logger.info("Hello from Acquisitions!");
+
+  res.status(200).send("Hello from Acquisitions!");
+});
 
 export default app;
