2d53646b1eba99b78b66e7175202d14d51541c43
Ranjeet Vishwakarma <ranjeetv.dev@gmail.com>
2026-04-22 15:55:46 +0530

    Implemented user sign-up functionality

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import router from "#routes/auth.routes.js";
 
 const app = express();
 app.use(helmet());
@@ -26,5 +27,21 @@ app.get("/", (req, res) => {
 
   res.status(200).send("Hello from Acquisitions!");
 });
+
+app.get("/health", (req, res) =>
+  res.status(200).json({
+    status: "OK",
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  }),
+);
+
+app.get("/api", (req, res) =>
+  res.status(200).json({
+    message: "Acquisitions API is running!",
+  }),
+);
+
+app.use("/api/auth", router);
 
 export default app;
